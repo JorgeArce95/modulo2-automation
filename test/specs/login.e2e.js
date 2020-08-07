fa7fd451af7f5801a76d1a51f231a13abf5dad39
Jorge Arce <arcejd95@gmail.com>
2020-08-07 15:48:08 -0600

    commit 5

---
+++ test/specs/login.e2e.js
@@ -0,0 +1,15 @@
+
+//las const son como los imports de jav a
+const LoginPage = require('../pageobjects/login.page');
+const NavigationPage = require('../pageobjects/navigation.page')
+
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        NavigationPage.open();
+        NavigationPage.goToSignIn();
+        console.log(NavigationPage.getConduitText());
+    });
+});
+
+
